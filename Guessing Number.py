177d37f00e5cd850a914b6cf770480074edad1bd
Abdulrahman Mohamed <abdokamr94@gmail.com>
2019-02-10 20:53:00 +0200

    Update Guessing Number.py

--- Guessing Number.py
+++ Guessing Number.py
@@ -1,64 +1,22 @@
 import random
-import string
-
-letter_input_1 = input('Choose a letter "v" for Vowels , "c" for Consonants , "o" for any other letter ')
-letter_input_2 = input('Choose a letter "v" for Vowels , "c" for Consonants , "o" for any other letter ')
-letter_input_3 = input('Choose a letter "v" for Vowels , "c" for Consonants , "o" for any other letter ')
-letter_input_4 = input('Choose a letter "v" for Vowels , "c" for Consonants , "o" for any other letter ')
-letter_input_5 = input('Choose a letter "v" for Vowels , "c" for Consonants , "o" for any other letter ')
-
-vowels = "aeiouy"
-consonants = "bcdfghjklmnpqrstvwxz"
-other = string.ascii_lowercase
-
-def generator():
-    # 1st
-    if (letter_input_1 == "v" or "V"):
-        letter1 = random.choice(vowels)
-    elif (letter_input_1 == "c" or "C"):
-        letter1 = random.choice(consonants)
-    elif (letter_input_1 == "o" or "O"):
-        letter1 = random.choice(other)
-    else:
-        letter1 = letter_input_1
-    # 2nd
-    if (letter_input_2 == "v" or "V"):
-        letter2 = random.choice(vowels)
-    elif (letter_input_2 == "c" or "C"):
-        letter2 = random.choice(consonants)
-    elif (letter_input_2 == "o" or "O"):
-        letter2 = random.choice(other)
-    else:
-        letter2 = letter_input_2
-    # 3th
-    if (letter_input_3 == "v" or "V"):
-        letter3 = random.choice(vowels)
-    elif (letter_input_3 == "c" or "C"):
-        letter3 = random.choice(consonants)
-    elif (letter_input_3 == "o" or "O"):
-        letter3 = random.choice(other)
-    else:
-        letter3 = letter_input_3
-    # 4th
-    if (letter_input_4 == "v" or "V"):
-        letter4 = random.choice(vowels)
-    elif (letter_input_4 == "c" or "C"):
-        letter4 = random.choice(consonants)
-    elif (letter_input_4 == "o" or "O"):
-        letter4 = random.choice(other)
-    else:
-        letter4 = letter_input_4
-    # 5th
-    if (letter_input_5 == "v" or "V"):
-        letter5 = random.choice(vowels)
-    elif (letter_input_5 == "c" or "C"):
-        letter5 = random.choice(consonants)
-    elif (letter_input_5 == "o" or "O"):
-        letter5 = random.choice(other)
-    else:
-        letter5 = letter_input_5
-    name = letter1 + letter2 + letter3 + letter4 + letter5
-    return (name)
-
-for i in range (23):
-    print(generator())
+def guessing():
+    randNum = random.randint(1 , 100)
+    isrunning = 1
+    number_guessing = 0
+    while isrunning:
+        number_guessing += 1
+        print("Hi human guess a number 1-100! \nYour Trial is " + str(number_guessing))
+        guess = input()
+        guess = int(guess)
+        
+        if guess > randNum:
+            print("your guess is high")
+    
+        if guess < randNum:
+            print("your guess is  low")
+    
+        if guess == randNum:
+            print("awsome you're a genius ^_^\nyou complete guessing in " + str(number_guessing) + " guesses\n")
+            isrunning = 0
+for i in range(100):
+    guessing()
