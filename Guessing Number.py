cc335d42886e8a2e4eefd6dea5c69d12c1132304
Abdulrahman Mohamed <abdokamr94@gmail.com>
2019-04-21 04:08:01 +0200

    Update Guessing Number.py

--- Guessing Number.py
+++ Guessing Number.py
@@ -2,11 +2,11 @@ import random
 def guessing():
     randNum = random.randint(1 , 100)
     number_guessing = 0
-    while True:
+    isrunning = 1
+    while isrunning:
         number_guessing += 1
         print("Hi human guess a number 1-100! \nYour Trial is " + str(number_guessing))
-        guess = input()
-        guess = int(guess)
+        guess = int(input())
         
         if guess > randNum:
             print("your guess is high")
@@ -15,8 +15,11 @@ def guessing():
             print("your guess is  low")
     
         if guess == randNum:
-            print("awsome you're a genius ^_^\nyou complete guessing in " + str(number_guessing) + " guesses\n")
-            isrunning = 0
+           print("awsome you're a genius ^_^\nyou complete guessing in {} guesses\n".format(number_guessing))
+           isrunning = 0
 
 while True:
     guessing()
+    
+    
+  
